381cbf718ed7e382f9973f64ae7fb8cd751c68b3
Lincon-Silva <131262719+Lincon-Silva@users.noreply.github.com>
2023-06-21 18:27:35 -0300

    Delete cypress directory

--- cypress/support/pages/home/elements.js
+++
@@ -1,5 +0,0 @@
-export const elements = {
-    menuWomen: ':nth-child(1) > .panel-heading > .panel-title > a',
-    categoryDress: '#Women > .panel-body > ul > :nth-child(1) > a',
-    elUrlDress: 'https://automationexercise.com/category_products/1'
-}
